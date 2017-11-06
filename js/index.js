e351bf83df179ef7ec95bf6e9a470df9ebe67325
huanglei <huanglei@bsoft.com.cn>
2017-11-06 18:03:51 +0800

    轮播图

--- js/index.js
+++ js/index.js
@@ -29,7 +29,31 @@ var search=function () {
   }
 }
 var banner =function () {
+    //大容器
+    var banner=document.querySelector(".jd_banner");
+    var width=banner.offsetWidth;
+    console.log(width)
+    //图片容器
+    var imagebox=banner.querySelector("ul:first-child");
+    //指示点容器
+    var pointBox=banner.querySelector("ul:last-child")
+    //所有的点
+    var points=pointBox.querySelectorAll("li");
+    // console.log(points)
+    //1,无缝滚动 无缝滑动（定时器 过渡 位移）
+    var index=1;
+    var timer=setInterval(function () {
+        index++;
+        if (index==5)
+            index=1;
+        // 加过渡
+        imagebox.style.transition='all 0.2s';
+        imagebox.style.webkitTransition='all 0.2s';
+        //位移
+        imagebox.style.transform='translateX('+(-index*width)+'px)';//位移轮播图宽度
+        imagebox.style.webkitTransform='translateX('+(-index*width)+'px)';//位移轮播图宽度
 
+    },2000)
 }
 var countDown=function () {
 
