e3ea48191048cdcbf472e5318459954fc4be52e5
huanglei <huanglei@bsoft.com.cn>
2017-11-10 18:00:34 +0800

    倒计时js

--- js/index.js
+++ js/index.js
@@ -135,5 +135,25 @@ var banner =function () {
     })
 }
 var countDown=function () {
+    var  time=60*60*11;//11个小时
+    var skTime=document.querySelector('.skill_time');
+    var spans=skTime.querySelectorAll('span');
 
+    var timer=setInterval(function () {
+        time--;
+        var h=Math.floor(time/3600);
+        var m=Math.floor(time%3600/60);
+        var s=time%60;
+        spans[0].innerHTML=Math.floor(h/10);
+        spans[1].innerHTML=h%10;
+
+        spans[3].innerHTML=Math.floor(m/10);
+        spans[4].innerHTML=m%10;
+
+        spans[6].innerHTML=Math.floor(s/10);
+        spans[7].innerHTML=s%10;
+        if(time<=0){
+            clearInterval(timer)
+        }
+    },1000)
 }
